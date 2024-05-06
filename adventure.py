7f1f426caeab1040aec9af13d07b44f3311ec72f
yoyo362 <159415611+yoyo362@users.noreply.github.com>
2024-05-05 21:57:01 -0400

    Add files via upload

--- adventure.py
+++ adventure.py
@@ -1,26 +1,19 @@
-import json
 import sys
+import json
 
-
-class TextAdventure:
+class AdventureGame:
     def __init__(self, map_file):
-        self.map_file = map_file
-        self.current_room = None
-        self.inventory = []
-        self.visited_rooms = set()
-        self.load_map()
+        self.load_map(map_file)
+        self.current_room = self.start_room
 
-    def load_map(self):
-        with open(self.map_file, 'r') as f:
-            try:
-                game_map = json.load(f)
-                self.validate_map(game_map)
-                self.rooms = {room['name']: room for room in game_map['rooms']}
-                self.current_room = game_map['start']
-            except json.JSONDecodeError:
-                sys.exit("Invalid JSON format in map file.")
-            except KeyError:
-                sys.exit("Map file is missing required keys.")
+    def load_map(self, map_file):
+        with open(map_file, 'r') as f:
+            game_map = json.load(f)
+        
+        self.validate_map(game_map)
+
+        self.start_room = game_map['start']
+        self.rooms = {room['name']: room for room in game_map['rooms']}
 
     def validate_map(self, game_map):
         if 'start' not in game_map or 'rooms' not in game_map:
@@ -33,6 +26,7 @@ class TextAdventure:
                 sys.exit("Duplicate room names found in map file.")
             room_names.add(room_name)
 
+        for room in game_map['rooms']:
             for exit_room in room['exits'].values():
                 if exit_room not in room_names:
                     sys.exit(f"Invalid exit room '{exit_room}' in map file.")
@@ -41,68 +35,49 @@ class TextAdventure:
             sys.exit(f"Invalid start room '{game_map['start']}' in map file.")
 
     def move(self, direction):
-        if direction in self.rooms[self.current_room]['exits']:
-            self.current_room = self.rooms[self.current_room]['exits'][direction]
-            self.visited_rooms.add(self.current_room)
-            self.print_room_description()
+        exit_room = self.current_room['exits'].get(direction)
+        if exit_room:
+            self.current_room = self.rooms[exit_room]
         else:
             print("You can't go that way.")
 
-    def print_room_description(self):
-        room = self.rooms[self.current_room]
+    def look(self):
+        room = self.current_room
+        print(room['name'])
         print(room['desc'])
-        if 'items' in room:
-            print("You see the following items in the room:")
-            for item in room['items']:
-                print(f"- {item}")
+        print("Exits:", ", ".join(room['exits'].keys()))
+        print("Items:", ", ".join(room['items']))
 
-    def take_item(self, item_name):
-        room = self.rooms[self.current_room]
-        if 'items' in room and item_name in room['items']:
-            self.inventory.append(item_name)
-            room['items'].remove(item_name)
-            print(f"You took the {item_name}.")
+    def get_item(self, item):
+        if item in self.current_room['items']:
+            print(f"You pick up the {item}.")
+            self.current_room['items'].remove(item)
         else:
-            print("There is no such item here.")
+            print(f"There is no {item} in this room.")
 
-    def drop_item(self, item_name):
-        if item_name in self.inventory:
-            self.inventory.remove(item_name)
-            self.rooms[self.current_room].setdefault('items', []).append(item_name)
-            print(f"You dropped the {item_name}.")
-        else:
-            print("You don't have that item.")
+    def inventory(self):
+        print("Inventory:", ", ".join(self.current_room['items']))
 
-    def print_inventory(self):
-        if self.inventory:
-            print("You are carrying the following items:")
-            for item in self.inventory:
-                print(f"- {item}")
-        else:
-            print("You are not carrying any items.")
-
-    def quit_game(self):
-        print("Goodbye!")
-        sys.exit()
-
+    def play(self):
+        while True:
+            command = input("What would you like to do? ").strip().lower()
+            if command == 'quit':
+                print("Goodbye!")
+                break
+            elif command == 'look':
+                self.look()
+            elif command.startswith('go '):
+                direction = command[3:]
+                self.move(direction)
+                self.look()
+            elif command.startswith('get '):
+                item = command[4:]
+                self.get_item(item)
+            elif command == 'inventory':
+                self.inventory()
+            else:
+                print("Invalid command.")
 
 if __name__ == "__main__":
-    adventure = TextAdventure("json.map")
-    adventure.print_room_description()
-    while True:
-        command = input("What would you like to do? ").strip().lower()
-        if command.startswith("go "):
-            direction = command[3:]
-            adventure.move(direction)
-        elif command.startswith("get ") or command.startswith("take "):
-            item_name = command[4:]
-            adventure.take_item(item_name)
-        elif command.startswith("drop "):
-            item_name = command[5:]
-            adventure.drop_item(item_name)
-        elif command == "inventory":
-            adventure.print_inventory()
-        elif command == "quit":
-            adventure.quit_game()
-        else:
-            print("Invalid command.")
+    game = AdventureGame("json.map")
+    game.play()
